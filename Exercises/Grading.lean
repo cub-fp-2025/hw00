469c9be4376140df8136c40fece922acdf751581
Komi Golova <komi.golov@jetbrains.com>
2025-09-07 10:53:02 +0200

    Add example exercise

--- Exercises/Grading.lean
+++ Exercises/Grading.lean
@@ -1,2 +1,7 @@
 import Exercises.Autograder
 import Exercises.Problems
+
+#eval Exercises00.student_name
+
+def ex1 : 2 + 2 = 4 := Exercises00.two_plus_two_is_four
+#print_grade_results ex1
--- Exercises/Problems.lean
+++ Exercises/Problems.lean
@@ -1,3 +1,7 @@
 namespace Exercises00
 
+def student_name: String := sorry
+
+def two_plus_two_is_four : 2 + 2 = 4 := sorry
+
 end Exercises00
